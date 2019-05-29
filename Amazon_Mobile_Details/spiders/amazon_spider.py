62b3cde0ecc3fc80fb1c5f4701b0867b398be1ad
Chinmayrane16 <chinmayrane16@gmail.com>
2019-05-29 22:46:46 +0530

    Added Code

--- Amazon_Mobile_Details/spiders/amazon_spider.py
+++ Amazon_Mobile_Details/spiders/amazon_spider.py
@@ -6,19 +6,21 @@ from ..items import AmazonMobileDetailsItem
 class AmazonSpiderSpider(scrapy.Spider):
     name = 'amazon_spider'
     allowed_domains = ['amazon.in']
+    # Crawl Mobiles for top 5 pages
     start_urls = ['https://www.amazon.in/s?k=Mobiles&ref=nb_sb_noss_2']
-
+    # initialize
     page_number = 2
 
     def parse(self, response):
         items = AmazonMobileDetailsItem()
 
+        # Extracting details
         mobile_name = response.css('.a-color-base.a-text-normal').css('::text').extract()
         mobile_review = response.css('.a-size-small .a-size-base').css('::text').extract()
         mobile_price = response.css('.a-price:nth-child(1) .a-price-whole').css('::text').extract()
         mobile_imagelink = response.css('.s-image::attr(src)').extract()
 
-
+        # Passing it to items dictionary
         items['mobile_name'] = mobile_name
         items['mobile_review'] = mobile_review
         items['mobile_price'] = mobile_price
@@ -26,6 +28,7 @@ class AmazonSpiderSpider(scrapy.Spider):
 
         yield items
 
+        # Parse subsequent pages
         next_page = 'https://www.amazon.in/s?k=Mobiles&page=' + str(AmazonSpiderSpider.page_number) + '&qid=1559110705&ref=sr_pg_2'
 
 
